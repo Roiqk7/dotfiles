2d73aa7105041a35bbbc6d17ae42226510bc389c
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-01-24 20:46:32 +0100

    Add files via upload

---
+++ .config/tmuxAliases.zsh
@@ -0,0 +1,7 @@
+# tmux aliases
+alias ta = "tmux attach"
+alias tls = "tmux ls"
+alias tat = "tmux attach -t"
+alias td = "tmux detach"
+alias tns = "tmux new-session -s"
+alias tks = "tmux kill-session"
