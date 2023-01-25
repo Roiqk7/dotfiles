1857947d96f74b399e943a0a30aa50b9cdd37148
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-01-25 06:01:33 +0100

    Add files via upload

---
+++ .config/aliases.zsh
@@ -0,0 +1,20 @@
+# reload zsh config
+alias c="clear"
+alias x="exit"
+alias r="source ~/.zshrc"
+
+# filesystem aliases
+alias .="open ."
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# tmux aliases
+alias ta="tmux attach"
+alias tl="tmux ls"
+alias ta="tmux attach -t"
+alias td="tmux detach"
+alias tns="tmux new-session -s"
+alias tks="tmux kill-session"
+alias tka="tmux kill-server"
