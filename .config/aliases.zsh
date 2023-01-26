f61070bce0c17d338557d45904c6832bf5092f3f
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-01-26 19:52:09 +0100

    Updated aliases

---
+++ .config/aliases.zsh
@@ -0,0 +1,25 @@
+# general
+alias r="source ~/.zshrc"
+alias c="clear"
+alias x="exit"
+
+# filesystem aliases
+alias .="open ."
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# brew aliases
+alias bu="brew upgrade"
+alias bi="brew install"
+alias bl="brew list"
+
+# git aliases
+alias gc="git clone --recursive"
+
+# tmux aliases
+alias td="tmux detach"
+alias tkc="tmux kill-session"
+alias tko="tmux kill-session -a"
+alias tka="tmux kill-server"
