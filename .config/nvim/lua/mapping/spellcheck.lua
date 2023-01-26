5d3f08ee08d04f445fb8465341418c90363d7875
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-01-26 19:20:51 +0100

    Delete .config/nvim directory

--- .config/nvim/lua/mapping/spellcheck.lua
+++
@@ -1,6 +0,0 @@
-require("utils")
--- === Spell checking === "
--- For help on spell checking :help spell
-
--- Pressing <leader>st will toggle and untoggle spell checking
-map({ "n", "v", "o" }, "<leader>st", "<cmd>set spell!<cr>", { remap = true })
