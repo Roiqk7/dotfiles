bb456ac7cdb5fb7aea68ee4b82e72a7c328c030a
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-02-04 14:22:09 +0100

    New aliases

--- .config/aliases.zsh
+++ .config/aliases.zsh
@@ -1,15 +1,16 @@
 # general
-alias r="source ~/.zshrc"
 alias c="clear"
 alias x="exit"
+alias n="nvim"
+alias r="source ~/.zshrc"
 alias u="bubu && maintain"
 
 # maintenance
 alias u_colorls="gem update colorls"
 alias u_omz="omz update"
-alias maintain="u_colorls && u_omz"
+alias maintain="u_colorls && u_omz && nonu"
 
-# file system aliases
+# file system
 alias ~="cd ~/"
 alias .="open ."
 alias ..="cd .."
@@ -27,19 +28,28 @@ alias aliases="nvim ~/.config/aliases.zsh"
 alias neovim="nvim ~/.config/nvim/init.lua"
 alias .tmux.conf="nvim ~/.tmux.conf"
 alias .p10k="nvim ~/.p10k.zsh"
+alias .config="cd ~/.config"
 alias work=""
 
-# brew aliases
+# brew
 alias bu="brew upgrade"
 alias bi="brew install"
 alias bri="brew reinstall"
 alias bl="brew list"
 
-# git aliases
+# git
 alias gc="git clone --recursive"
 
-# tmux aliases
+# tmex
+alias te="tmex -l"
+
+# tmux
+alias t="tmux"
 alias td="tmux detach"
 alias tkc="tmux kill-session"
 alias tko="tmux kill-session -a"
 alias tka="tmux kill-server"
+
+# # npm
+alias ni="npm install"
+alias nonu="npm outdated && npm update"
