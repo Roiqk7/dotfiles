455d2bbaa3f589d08c63ac0f5d38fb8ca27c16f9
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-02-04 20:53:47 +0100

    Delete .config/nvim/lua directory

--- .config/nvim/lua/mapping/init.lua
+++
@@ -1,7 +0,0 @@
-require("mapping.editor")
-require("mapping.lspconfig")
-require("mapping.lspsaga")
-require("mapping.movement")
-require("mapping.plugin")
-require("mapping.terminal")
-require("mapping.whichkey")
