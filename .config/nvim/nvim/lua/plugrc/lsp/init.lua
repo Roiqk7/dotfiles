d1902239a5fd821203da7aab895be681a70a8649
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-02-05 13:32:53 +0100

    Delete .config/nvim directory

--- .config/nvim/nvim/lua/plugrc/lsp/init.lua
+++
@@ -1,7 +0,0 @@
-require("plugrc.lsp.setup")
-require("plugrc.lsp.mason")
-require("plugrc.lsp.config")
-require("plugrc.lsp.null-ls")
-require("plugrc.lsp.signature")
-require("plugrc.lsp.status")
-require("plugrc.lsp.servers")
