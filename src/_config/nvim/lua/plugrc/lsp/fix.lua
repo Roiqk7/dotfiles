50c6e62594016908ad31ed7503b2914da40349ed
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-02-20 06:03:58 +0100

    Delete src/_config/nvim directory

--- src/_config/nvim/lua/plugrc/lsp/fix.lua
+++
@@ -1,4 +0,0 @@
--- fixes clang bug
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.offsetEncoding = "utf-16"
-require("lspconfig").clangd.setup({ capabilities = capabilities })
