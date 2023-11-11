60fe5f51b49885ed06116e788980d8bfce01b4e5
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2023-11-11 09:14:33 +0100

    Update aliases.zsh

--- src/_config/aliases.zsh
+++ src/_config/aliases.zsh
@@ -1,57 +1,72 @@
 # general
-alias c="clear"
-alias x="exit"
-alias n="nvim"
-alias r="source ~/.zshrc"
-alias u="bubu && maintain"
+ alias c="clear"
+ alias x="exit"
+ alias v="vim"
+ alias n="nvim"
+ alias r="source ~/.zshrc"
+ alias u="bubu && maintain"
+ alias py="python3"
+ alias exe="function _exe() { g++ -std=c++17 -o "$1" "$1.cpp" && ./"$1"; }; _exe"
 
-# maintenance
-alias u_colorls="gem update colorls"
-alias u_omz="omz update"
-alias maintain="u_colorls && u_omz && nonu"
+ # softwareupdate
+ alias su="softwareupdate"
+ alias sua="su --install --all"
 
-# file system
-alias ~="cd ~/"
-alias .="open ."
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias ls="colorls"
-alias tree="colorls --tree=3 --sd --dark"
-alias utree="colorls --tree=10 --sd --dark"
-alias detail="colorls -lA --sd --dark"
+ # maintenance
+ alias u_colorls="gem update colorls"
+ alias u_omz="omz update"
+ alias maintain="sua && u_colorls && u_omz && nonu"
 
-# shortcuts
-alias desk="cd ~/Desktop"
-alias .zshrc="nvim ~/.zshrc"
-alias aliases="nvim ~/.config/aliases.zsh"
-alias neovim="nvim ~/.config/nvim/init.lua"
-alias .tmux.conf="nvim ~/.tmux.conf"
-alias .p10k="nvim ~/.p10k.zsh"
-alias .config="cd ~/.config"
-alias work="cd ~/Desktop/Programming/C:C++/"
+ # file system
+ alias ~="cd ~/"
+ alias .="open ."
+ alias ..="cd .."
+ alias ...="cd ../.."
+ alias ....="cd ../../.."
+ alias .....="cd ../../../.."
+ alias ls="colorls"
+ alias tree="colorls --tree=3 --sd --dark"
+ alias utree="colorls --tree=10 --sd --dark"
+ alias detail="colorls -lA --sd --dark"
 
-# brew
-alias bu="brew upgrade"
-alias bi="brew install"
-alias bri="brew reinstall"
-alias bl="brew list"
+ # shortcuts
+ alias aliases="vim ~/.config/aliases.zsh"
+ alias home="cd ~/"
+ alias desk="cd ~/Desktop"
+ alias downloads="cd ~/Downloads"
+ alias neovim="vim ~/.config/nvim/init.lua"
+ alias work="cd ~/Desktop/Programming/C:C++/C++/Era2/NumeroDuo"
+ alias .zshrc="nvim ~/.zshrc"
+ alias .tmux.conf="vim ~/.tmux.conf"
+ alias .p10k="nvim ~/.p10k.zsh"
+ alias .config="cd ~/.config"
 
-# git
-alias gc="git clone --recursive"
-alias repo="gc https://github.com/Roiqk7/dotfiles.git"
+ # functional
+ alias copyLastCmd="fc -ln -1 | pbcopy"
+ alias pi="pip3 install"
+ alias search="grep"
+ alias searchall="grep -rl"
 
-# tmex
-alias te="tmex -l"
+ # brew
+ alias bu="brew upgrade"
+ alias bi="brew install"
+ alias bri="brew reinstall"
+ alias bl="brew list"
 
-# tmux
-alias t="tmux"
-alias td="tmux detach"
-alias tkc="tmux kill-session"
-alias tko="tmux kill-session -a"
-alias tka="tmux kill-server"
+ # git
+ alias gc="git clone --recursive"
+ alias repo="gc https://github.com/Roiqk7/dotfiles.git"
 
-# # npm
-alias ni="npm install"
-alias nonu="npm outdated && npm update"
+ # tmex
+ alias te="tmex -l"
+
+ # tmux
+ alias t="tmux"
+ alias td="tmux detach"
+ alias tkc="tmux kill-session"
+ alias tko="tmux kill-session -a"
+ alias tka="tmux kill-server"
+
+ # # npm
+ alias ni="npm install"
+ alias nonu="npm outdated && npm update"
